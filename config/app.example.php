d2fa5cca45ee6074469355995d17ea9c415de255
Mark <dereuromark@users.noreply.github.com>
2025-08-24 23:32:45 +0000

    Auto-fix CS - CS master branch

--- config/app.example.php
+++ config/app.example.php
@@ -3,8 +3,7 @@
 return [
 	'QueueScheduler' => [
 		// Additional plugins that are not loaded, but should be included, use `-` prefix to exclude
-		'plugins' => [
-		],
+		'plugins' => [],
 		'allowRaw' => false, // By default, this is only enabled in debug mode for security reasons.
 		'crontabMinimum' => '1 minute', // By default, this should be run as `* * * * *`
 	],
